cb75c9611e2467547da48977334af5cc871caafd
Hovhannes Amirjanyan <amirjanyanit@gmail.com>
2023-11-20 01:59:29 +0400

    board logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import './store';
+import { observer } from "mobx-react";
+
+import { store } from "./store";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    store.initialize();
+    window.addEventListener("keyup", (e) => {
+      if(e.key === 'e') {
+        store.moveBoard();
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
+      {store.board.map((c, cI) => (
+        <div className="line" key={cI}>
+          {c.map((i, iI) => (
+            <div key={iI} className="item">
+              {i}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default observer(App);
--- src/store.ts
+++ src/store.ts
@@ -8,28 +8,34 @@ import { makeAutoObservable } from "mobx";
 */
 
 type ObjectsT = "e" | "x" | "o" | "p";
-type RatesT = {type: ObjectsT, rate: number}[];
+type RatesT = { type: ObjectsT; rate: number }[];
 
 class Store {
   board: ObjectsT[][] = [];
-  scale = 30;
+  scale = 10;
+  itemsAtLine = 5;
 
   rates: RatesT = [
-    { type: "e", rate: 0.7 },
+    { type: "e", rate: 0 },
     { type: "x", rate: 0.1 },
     { type: "o", rate: 0.2 },
   ];
 
+  constructor() {
+    makeAutoObservable(this);
+    this.initialize();
+  }
+
   initialize() {
-    for (let i = 0; i <= this.scale * 3; i++) {
+    for (let i = 0; i < this.scale * 3; i++) {
       this.board[i] = [];
-      for (let j = 0; j <= this.scale; j++) {
+      for (let j = 0; j < this.scale; j++) {
         this.board[i][j] = "e";
       }
     }
   }
 
-  genItem() {
+  private genItem() {
     const totalRate = this.rates.reduce((total, { rate }) => total + rate, 0);
 
     const randomValue = Math.random() * totalRate;
@@ -42,16 +48,49 @@ class Store {
       }
     }
 
-    return null;
+    return "x";
   }
 
-  
+  private createNewLine() {
+    const line: ObjectsT[] = [];
+    for (let j = 0; j < this.scale; j++) {
+      line.push("e");
+    }
 
-  
+    this.board[0] = line;
+  }
 
-  constructor() {
-    makeAutoObservable(this);
-    this.initialize();
+  private setItemInitialPosition(round: number) {
+    for (let i = 0; i < round; i++) {
+      let emptyPositions: number[] = [];
+
+      this.board[0].forEach((c, i) => {
+        if (c === "e") {
+          emptyPositions.push(i);
+        }
+      });
+
+      let position = Math.floor(Math.random() * emptyPositions.length);
+
+      const pickedPosition = emptyPositions[position];
+
+      this.board[0][pickedPosition] = this.genItem();
+    }
+
+    return true;
+  }
+
+  public moveBoard() {
+    let lastLine = this.board[this.board.length - 1];
+
+    for (let i = this.board.length - 1; i >= 0; i--) {
+      this.board[i] = this.board[i - 1];
+    }
+
+    this.createNewLine();
+    this.setItemInitialPosition(this.itemsAtLine);
+
+    return lastLine;
   }
 }
 
